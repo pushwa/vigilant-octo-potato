086a7a2d94e1e563b881ea46a6d2f8c4e974ba90
Vidar Korneliussen <vidar@pushwagner.net>
2021-03-27 20:12:43 +0100

    ---

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Welcome' });
+  res.render('index', { title: 'Welcome', buttonText: 'Hide/Show Object' });
 });
 
 module.exports = router;
