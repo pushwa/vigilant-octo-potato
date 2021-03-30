4bfa4a63b34fb35d6a978547d29afc28b60cdff1
Vidar Korneliussen <vidar@pushwagner.net>
2021-03-30 15:08:02 +0200

    ------------

--- public/js/test.js
+++
@@ -1,8 +0,0 @@
-//
-function updateCamera() {
-  let body = document.querySelector('body');
-  camera.position.y = 0 + window.scrollY / 150;
-  camera.position.z = 10 + window.scrollY / -150;
-}
-
-window.addEventListener('scroll', updateCamera);
